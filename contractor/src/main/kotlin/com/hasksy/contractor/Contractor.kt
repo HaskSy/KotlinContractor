d823c80f3c688fcc3569f23867581aab556b2f11
Sergo Kovaltsov <sergeykovaltsov@gmail.com>
2024-02-08 04:50:57 +0300

    test another

--- contractor/src/main/kotlin/com/hasksy/contractor/Contractor.kt
+++ contractor/src/main/kotlin/com/hasksy/contractor/Contractor.kt
@@ -67,7 +67,7 @@ fun <T> dbc(block: ContractContext.() -> T): T {
     } finally {
         isVerifying = true
 
-        val messageList: MutableList<String> = mutableListOf()
+        val messageList: MutableList<String> = mutableListOf("List of Contract Violations:\n")
 
         requiresList.forEachIndexed { index, lambda ->
             if (!lambda()) messageList.add("[Requires Error]: Pre-condition №${index+1} not satisfied")
@@ -77,10 +77,80 @@ fun <T> dbc(block: ContractContext.() -> T): T {
             if (!ensuresCtx.lambda()) messageList.add("[Ensures Error ]: Post-condition №${index+1} not satisfied")
         }
 
-        if (messageList.size > 0) {
+        if (messageList.size > 1) {
             throw ContractInvalidationException(
-                "List of Contract Violations:\n" + messageList.joinToString("\n")
+                messageList.joinToString("\n")
             )
         }
     }
 }
+
+//class Contextualized() {
+//    var savedCount = 0; var loadedCount = 0
+//    var isVerifying = false
+//    val stash : MutableList<Any> = mutableListOf()
+//    val requiresList: MutableList<() -> Boolean> = mutableListOf()
+//    val ensuresList: MutableList<EnsuresContext.() -> Boolean> = mutableListOf()
+//
+//    val ensuresCtx: EnsuresContext = object : EnsuresContext {
+//        override fun <U : Any> old(value: U): U {
+//            if (!isVerifying) {
+//                stash.add(savedCount++, value)
+//                return value
+//            }
+//            @Suppress("UNCHECKED_CAST") // should never occur
+//            return stash[loadedCount++] as U
+//        }
+//    };
+//    val ctx: ContractContext = object : ContractContext {
+//        override fun requires(precondition: () -> Boolean) {
+//            requiresList.add(precondition)
+//            if (!precondition()) {
+//                System.err.println("Precondition is not satisfied, unexpected behaviour might occur")
+////                logger.warn { "Precondition is not satisfied, unexpected behaviour might occur" }
+//            }
+//        }
+//
+//        override fun ensures(postcondition: EnsuresContext.() -> Boolean) {
+//            ensuresList.add(postcondition)
+//            ensuresCtx.postcondition() // If there's function 'old' - calls it
+//        }
+//    };
+//
+//}
+//fun contract(init: ContractContext.() -> Unit) : Contextualized? {
+//    if (!KotlinContractorAssertions.ENABLED) {
+//        return null
+//    }
+//
+//    val objectCtx = Contextualized()
+//    objectCtx.ctx.init();
+//    return objectCtx;
+//}
+//
+//infix fun <T> Contextualized?.execute(body: () -> T) : T {
+//    if (this == null) {
+//        return body();
+//    }
+//    try {
+//        return body();
+//    } finally {
+//        isVerifying = true
+//
+//        val messageList: MutableList<String> = mutableListOf("List of Contract Violations:\n")
+//
+//        requiresList.forEachIndexed { index, lambda ->
+//            if (!lambda()) messageList.add("[Requires Error]: Pre-condition №${index+1} not satisfied")
+//        }
+//
+//        ensuresList.forEachIndexed { index, lambda ->
+//            if (!ensuresCtx.lambda()) messageList.add("[Ensures Error ]: Post-condition №${index+1} not satisfied")
+//        }
+//
+//        if (messageList.size > 1) {
+//            throw ContractInvalidationException(
+//                messageList.joinToString("\n")
+//            )
+//        }
+//    }
+//}
